1421f87b2dd94bcf7e837aaab48b271a36db1b17
OnumaKenya <kenyaonuma@gmail.com>
2023-04-23 22:12:12 +0900

    initial

--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
-import pandas as pd
 import streamlit as st
-from itertools import permutations
-from solver import TSP2D
-import numpy as np
-import matplotlib.pyplot as plt
+from solver import TSP2D, LocalSearch, kick_double_bridge, nearest_neighbor_tour
 
-n = 10
-tsp_data = TSP2D.make_random_data(n)
-opt_len = np.inf
-opt_tour = None
+n = st.number_input("number of points", min_value=20, max_value=1000, value=100, step=1)
+ls_iterations = st.number_input("number of iterations", min_value=1, max_value=50, value=10, step=1)
+random_seed = st.number_input("random seed", min_value=1, value=42, step=1)
+L = st.number_input("size of distance list", min_value=10, max_value=1000, value=20, step=1)
 
-placeholder = st.empty()
-placeholder_tour = st.empty()
-placeholder_obj = st.empty()
+if st.button("Go!"):
+    tsp_data = TSP2D.make_random_data(n, random_seed=random_seed)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Current Opt Tour")
+        placeholder_opt = st.empty()
+        placeholder_opt_obj = st.empty()
+    with col2:
+        st.subheader("Current Local Search")
+        placeholder = st.empty()
+        placeholder_obj = st.empty()
 
-for tour in permutations(range(1, n)):
-    if tsp_data.length_of_tour(list(tour)) < opt_len:
-        opt_len = tsp_data.length_of_tour(list(tour))
-        opt_tour = tour
-        tsp_data.plot_tour(list(opt_tour), placeholder=placeholder)
-        placeholder_tour.info(f"current tour:{(0,) + opt_tour}")
-        placeholder_obj.info(f"current objective: {opt_len}")
+    ls = LocalSearch(tsp_data, L=L)
+    ls.build_dists()
+    opt_tour = nearest_neighbor_tour(tsp_data)
+    tsp_data.plot_tour(opt_tour, placeholder_opt)
+    placeholder_opt_obj.info(f"tour length: {tsp_data.length_of_tour(opt_tour): .3f} (initial)")
 
-st.success("Optimization Done!")
+    init_tour = opt_tour
 
+    def ordinal(num):
+        return "%d%s" % (num, "tsnrhtdd"[(num / 10 % 10 != 1) * (num % 10 < 4) * num % 10 :: 4])
+
+    for i in range(1, ls_iterations + 1):
+        tour = ls.local_search(init_tour, placeholder)
+        placeholder_obj.info(f"tour length: {tsp_data.length_of_tour(tour): .3f} ({ordinal(i)} iteration)")
+        if tsp_data.length_of_tour(tour) < tsp_data.length_of_tour(opt_tour):
+            opt_tour = tour
+            tsp_data.plot_tour(opt_tour, placeholder_opt)
+            placeholder_opt_obj.info(f"tour length: {tsp_data.length_of_tour(opt_tour) :.3f} ({ordinal(i)} iteration)")
+        init_tour = kick_double_bridge(tour)
+    st.success("Optimization Done!")
--- src/solver.py
+++ src/solver.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-import time
+import bisect
 from dataclasses import dataclass
 from math import dist
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas import DataFrame, Series
-import matplotlib.pyplot as plt
+from pandas import Series
 
 
 @dataclass
@@ -46,7 +47,9 @@ class TSP2D:
         if tour is None:
             tour_full = list(range(self.n)) + [0]
         else:
-            tour_full = [0] + tour + [0]
+            if len(tour) != self.n:
+                raise RuntimeError
+            tour_full = tour + [tour[0]]
 
         x = self.x.values[tour_full]
         y = self.y.values[tour_full]
@@ -54,7 +57,7 @@ class TSP2D:
         plt.close()
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
-        ax.plot(x, y, marker=".", markersize=15)
+        ax.plot(x, y, marker=".")
         if placeholder is None:
             st.pyplot(fig)
         else:
@@ -68,3 +71,156 @@ class TSP2D:
             p2 = (self.x[tour_full[i + 1]], self.y[tour_full[i + 1]])
             ret += dist(p1, p2)
         return ret
+
+
+def nearest_neighbor_tour(data: TSP2D) -> list:
+    """return tour by nearest neighbor
+
+    Args:
+        data: condition of tsp
+
+    Returns:
+        tour by nearest neighbor
+    """
+    tour = [0]
+    appended = set(tour)
+    x = data.x
+    y = data.y
+    n = data.n
+    for _ in range(n - 1):
+        p1 = (x[tour[-1]], y[tour[-1]])
+        p2_dist = np.inf
+        p2 = -1
+        for i in range(n):
+            if i in appended:
+                continue
+            tmp = (x[i], y[i])
+            if dist(p1, tmp) < p2_dist:
+                p2 = i
+                p2_dist = dist(p1, tmp)
+
+        tour.append(p2)
+        appended.add(p2)
+    return tour
+
+
+class LocalSearch:
+    data: TSP2D
+    tour: list
+    L: int
+    dists: list
+
+    def __init__(self, data: TSP2D, L: int | None = None):
+        self.data = data
+        n = data.n
+        self.tour = list(range(n))
+        if L is None:
+            self.L = n
+        else:
+            self.L = L
+
+        self.dists = [[] for _ in range(n)]
+
+    def build_dists(self):
+        n = self.data.n
+        x = self.data.x
+        y = self.data.y
+        L = self.L
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                d = dist((x[i], y[i]), (x[j], y[j]))
+                bisect.insort(self.dists[i], (d, j))
+                if len(self.dists[i]) > L:
+                    self.dists[i] = self.dists[i][:L]
+
+    def local_search(self, init_tour: list, placeholder) -> list:
+        tour = init_tour
+        v1 = 0
+        data = self.data
+        n = data.n
+        x = data.x
+        y = data.y
+
+        while True:
+            flg_improve = False
+            # 始点が片方の探索
+            for _ in range(n):
+                idx_v1 = tour.index(v1)
+                idx_v2 = (idx_v1 + 1) % n
+                v2 = tour[idx_v2]
+                d12 = dist((x[v1], y[v1]), (x[v2], y[v2]))
+                for d14, v4 in self.dists[v1]:
+                    if d14 >= d12:
+                        break
+                    idx_v4 = tour.index(v4)
+                    idx_v3 = (idx_v4 + 1) % n
+                    v3 = tour[idx_v3]
+                    if len({v1, v2, v3, v4}) != 4:
+                        continue
+                    d34 = dist((x[v3], y[v3]), (x[v4], y[v4]))
+                    d23 = dist((x[v2], y[v2]), (x[v3], y[v3]))
+
+                    if d23 + d14 < d12 + d34:
+                        flg_improve = True
+                        if idx_v2 < idx_v4:
+                            tour = tour[:idx_v2] + list(reversed(tour[idx_v2 : idx_v4 + 1])) + tour[idx_v4 + 1 :]
+                        else:
+                            tour = tour[:idx_v3] + list(reversed(tour[idx_v3 : idx_v1 + 1])) + tour[idx_v1 + 1 :]
+                        break
+                if flg_improve:
+                    break
+
+                for d23, v3 in self.dists[v2]:
+                    idx_v3 = tour.index(v3)
+                    idx_v4 = (idx_v3 + n - 1) % n
+                    v4 = tour[idx_v4]
+                    if len({v1, v2, v3, v4}) != 4:
+                        continue
+                    d34 = dist((x[v3], y[v3]), (x[v4], y[v4]))
+                    d14 = dist((x[v1], y[v1]), (x[v4], y[v4]))
+                    if d23 >= d34:
+                        break
+                    if d23 + d14 < d12 + d34:
+                        flg_improve = True
+                        if idx_v2 < idx_v4:
+                            tour = tour[:idx_v2] + list(reversed(tour[idx_v2 : idx_v4 + 1])) + tour[idx_v4 + 1 :]
+                        else:
+                            tour = tour[:idx_v3] + list(reversed(tour[idx_v3 : idx_v1 + 1])) + tour[idx_v1 + 1 :]
+                        break
+                v1 = (v1 + 1) % n
+                if flg_improve:
+                    break
+            if flg_improve:
+                # print(data.length_of_tour(tour))
+                data.plot_tour(tour, placeholder)
+                continue
+            else:
+                break
+
+        return tour
+
+
+def kick_double_bridge(tour: list) -> list:
+    used = set()
+    n = len(tour)
+    kick_edges = []
+    while len(kick_edges) < 4:
+        u = np.random.randint(n)
+        if u not in used and (u + 1) % n not in used:
+            kick_edges.append((u, (u + 1) % n))
+            used |= {u, (u + 1) % n}
+
+    kick_edges.sort()
+    ret = []
+    for i in range(2, -1, -1):
+        for j in range(kick_edges[i][1], kick_edges[i + 1][0] + 1):
+            ret.append(tour[j])
+
+    if kick_edges[3][1] == 0:
+        ret += tour[: kick_edges[0][0] + 1]
+    else:
+        ret += tour[kick_edges[3][1] :] + tour[: kick_edges[0][0] + 1]
+    return ret
