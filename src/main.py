14f248f919c7deaa7cdde851d1cf30bfd0272cf3
kenya-onuma-0882215 <onuma.kenya.ma@mitsubishichem-sys.co.jp>
2023-04-26 17:30:31 +0900

    fix logic

--- src/main.py
+++ src/main.py
@@ -1,34 +1,43 @@
-import streamlit as st
-from solver import TSP2D, LocalSearch, kick_double_bridge, nearest_neighbor_tour
 import matplotlib.pyplot as plt
-st.header("Solve TSP by ILS")
+import numpy as np
+import streamlit as st
+from solver import TSP2D, LocalSearch, kick_double_bridge, nearest_neighbor_tour, randomized_nearest_neighbor_tour
 
-n = st.number_input("number of points", min_value=20, max_value=3000, value=100, step=1)
-ls_iterations = st.number_input("number of iterations", min_value=1, max_value=50, value=10, step=1)
-random_seed = st.number_input("random seed", min_value=1, value=42, step=1)
-L = st.number_input("size of distance list", min_value=10, max_value=1000, value=20, step=1)
+st.header("Solve TSP by LS")
+
+data_flg = st.selectbox("select data type", ("Japan Data", "Make Random"))
+if data_flg == "Make Random":
+    n = st.number_input("number of points", min_value=20, max_value=3000, value=100, step=1)
+method = st.selectbox("select method", ("ILS", "GRASP"))
+ls_iterations = st.number_input("number of iterations", min_value=1, max_value=50, value=30, step=1)
+random_seed = st.number_input("random seed", min_value=0, value=42, step=1)
 
 if st.button("Go!"):
-    tsp_data = TSP2D.make_random_data(n, random_seed=random_seed)
+    np.random.seed(random_seed)
+    if data_flg == "Make Random":
+        tsp_data = TSP2D.make_random_data(n)
+    else:
+        tsp_data = TSP2D.read_japan_data()
+
     col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("Current Opt Tour")
-        placeholder_opt = st.empty()
-        placeholder_opt_obj = st.empty()
-    with col2:
-        st.subheader("Current Local Search")
-        placeholder = st.empty()
-        placeholder_obj = st.empty()
+    st.subheader("Current Opt Tour")
+    placeholder_opt = st.empty()
+    placeholder_opt_obj = st.empty()
+    st.subheader("Current Local Search")
+    placeholder = st.empty()
+    placeholder_obj = st.empty()
     st.subheader("length of tour")
     obj_plot = st.empty()
     obj_plot2 = st.empty()
-
-    ls = LocalSearch(tsp_data, L=L)
-    ls.build_dists()
-
-    opt_tour = nearest_neighbor_tour(tsp_data)
+    if method == "ILS":
+        opt_tour = nearest_neighbor_tour(tsp_data)
+    else:
+        opt_tour = randomized_nearest_neighbor_tour(tsp_data)
     tsp_data.plot_tour(opt_tour, placeholder_opt)
-    placeholder_opt_obj.info(f"tour length: {tsp_data.length_of_tour(opt_tour): .3f} (initial)")
+    placeholder_opt_obj.info(f"tour length: {tsp_data.length_of_tour(opt_tour)} (initial)")
+
+    ls = LocalSearch(tsp_data)
+    ls.build_dists()
 
     init_tour = opt_tour
     itr_num = []
@@ -37,7 +46,7 @@ if st.button("Go!"):
 
     for i in range(1, ls_iterations + 1):
         tour = ls.local_search(init_tour, placeholder)
-        placeholder_obj.info(f"tour length: {tsp_data.length_of_tour(tour): .3f} (iteration No.{i})")
+        placeholder_obj.info(f"tour length: {tsp_data.length_of_tour(tour)} (iteration No.{i})")
         tour_len = tsp_data.length_of_tour(tour)
 
         itr_num.append(i)
@@ -46,7 +55,7 @@ if st.button("Go!"):
             opt_tour = tour
             opt_len = tour_len
             tsp_data.plot_tour(opt_tour, placeholder_opt)
-            placeholder_opt_obj.info(f"tour length: {tour_len :.3f} (iteration No.{i})")
+            placeholder_opt_obj.info(f"tour length: {tour_len} (iteration No.{i})")
 
         plt.close()
         fig = plt.figure()
@@ -64,5 +73,8 @@ if st.button("Go!"):
         ax.plot(list(range(len(ls.history))), ls.history)
         obj_plot2.pyplot(fig)
 
-        init_tour = kick_double_bridge(tour)
+        if method == "ILS":
+            init_tour = kick_double_bridge(tour)
+        else:
+            init_tour = randomized_nearest_neighbor_tour(tsp_data)
     st.success("Optimization Done!")
--- src/solver.py
+++ src/solver.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import bisect
 from dataclasses import dataclass
 from math import dist
 
@@ -8,7 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas import Series
+from pandas import DataFrame, Series
+from scipy.spatial import distance
 
 
 @dataclass
@@ -16,27 +16,40 @@ class TSP2D:
     n: int
     x: Series
     y: Series
+    dists: DataFrame
 
     @staticmethod
-    def make_random_data(n: int, xrange: int = 1000, yrange: int = 1000, random_seed: int = None) -> TSP2D:
+    def make_random_data(n: int, xrange: int = 1000, yrange: int = 1000) -> TSP2D:
         """make random 2d tsp data
 
         Args:
             n: number of points
             xrange: maximum x-coordinate
             yrange: maximum y-coordinate
-            random_seed: random seed
         Returns:
             random 2D tsp data
         """
-        if random_seed is not None:
-            np.random.seed(random_seed)
-
         x = pd.Series(np.random.randint(xrange, size=n))
         y = pd.Series(np.random.randint(yrange, size=n))
-        return TSP2D(n=n, x=x, y=y)
+        coor = np.array([x, y]).T
+        dists = pd.DataFrame(distance.cdist(coor, coor, metric="euclidean")).round()
+        return TSP2D(n=n, x=x, y=y, dists=dists)
 
-    def plot_tour(self, tour: list | None = None, placeholder=None) -> None:
+    @staticmethod
+    def read_japan_data() -> TSP2D:
+        """read japan tsp data
+        Returns:
+            japan tsp data
+        """
+        df = pd.read_csv("data/ja9847.csv")
+        n = len(df)
+        x = df["y"]
+        y = df["x"]
+        coor = np.array([x, y]).T
+        dists = pd.DataFrame(distance.cdist(coor, coor, metric="euclidean")).round()
+        return TSP2D(n=n, x=x, y=y, dists=dists)
+
+    def plot_tour(self, tour: list | None = None, placeholder=None, v1=None, v4=None) -> None:
         """plot tour
 
         Args:
@@ -57,19 +70,28 @@ class TSP2D:
         plt.close()
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
-        ax.plot(x, y, marker=".")
+        ax.set_aspect("equal")
+        ax.plot(x, y, linewidth=0.5)
+        if v1 is not None:
+            idx_1 = tour_full.index(v1)
+            idx_2 = idx_1 + 1
+
+            idx_4 = tour_full.index(v4)
+            idx_3 = idx_4 + 1
+            ax.plot([x[idx_1], x[idx_2]], [y[idx_1], y[idx_2]], linewidth=1.5, color="orange")
+            ax.plot([x[idx_4], x[idx_3]], [y[idx_4], y[idx_3]], linewidth=1.5, color="orange")
         if placeholder is None:
             st.pyplot(fig)
         else:
             placeholder.pyplot(fig)
 
     def length_of_tour(self, tour: list) -> float:
-        ret = 0.0
+        ret = 0
         tour_full = tour + [tour[0]]
         for i in range(self.n):
             p1 = (self.x[tour_full[i]], self.y[tour_full[i]])
             p2 = (self.x[tour_full[i + 1]], self.y[tour_full[i + 1]])
-            ret += dist(p1, p2)
+            ret += round(dist(p1, p2))
         return ret
 
 
@@ -83,24 +105,42 @@ def nearest_neighbor_tour(data: TSP2D) -> list:
         tour by nearest neighbor
     """
     tour = [0]
-    appended = set(tour)
-    x = data.x
-    y = data.y
     n = data.n
-    for _ in range(n - 1):
-        p1 = (x[tour[-1]], y[tour[-1]])
-        p2_dist = np.inf
-        p2 = -1
-        for i in range(n):
-            if i in appended:
-                continue
-            tmp = (x[i], y[i])
-            if dist(p1, tmp) < p2_dist:
-                p2 = i
-                p2_dist = dist(p1, tmp)
+    dists = data.dists
+    flg = [True] * n
+    flg[0] = False
 
-        tour.append(p2)
-        appended.add(p2)
+    for _ in range(n - 1):
+        tour.append(dists.loc[tour[-1], flg].idxmin())
+        flg[tour[-1]] = False
+    return tour
+
+
+def randomized_nearest_neighbor_tour(data: TSP2D, alpha: float = 0.0025) -> list:
+    """return tour by randomized nearest neighbor
+
+    Args:
+        data: condition of tsp
+        alpha: threshold
+
+    Returns:
+        tour by nearest neighbor
+    """
+    n = data.n
+    tour = [np.random.randint(n)]
+    dists = data.dists
+    flg = [True] * n
+    flg[tour[-1]] = False
+
+    for _ in range(n - 1):
+        points = dists.loc[0, flg].index
+        dists_list = dists.loc[tour[-1], points]
+        dist_max = dists_list.max()
+        dist_min = dists_list.min()
+        dist_threshold = dist_min + alpha * (dist_max - dist_min)
+        candidates = dists_list.loc[dists_list <= dist_threshold].index
+        tour.append(np.random.choice(candidates))
+        flg[tour[-1]] = False
     return tour
 
 
@@ -108,7 +148,7 @@ class LocalSearch:
     data: TSP2D
     tour: list
     L: int
-    dists: list
+    dists_list: list
     do_not_look: list
     history: list
 
@@ -121,43 +161,37 @@ class LocalSearch:
         else:
             self.L = L
 
-        self.dists = [[] for _ in range(n)]
+        self.dists_list = None
         self.history = []
 
     def build_dists(self):
         n = self.data.n
-        x = self.data.x
-        y = self.data.y
+        dists = self.data.dists
         L = self.L
 
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    continue
-                d = dist((x[i], y[i]), (x[j], y[j]))
-                bisect.insort(self.dists[i], (d, j))
-                if len(self.dists[i]) > L:
-                    self.dists[i] = self.dists[i][:L]
+        self.dists_list = [dists[i].nsmallest(L + 1).iloc[1:] for i in range(n)]
 
     def local_search(self, init_tour: list, placeholder) -> list:
         tour = init_tour
-        v1 = -1
+        i = 0
         data = self.data
         n = data.n
+        perm = np.random.permutation(list(range(n)))
         self.history.append(data.length_of_tour(init_tour))
         self.do_not_look = [False] * n
         while True:
             flg_improve = False
             # 始点を固定しての探索
             for _ in range(n):
-                v1 = (v1 + 1) % n
+                i = (i + 1) % n
+                v1 = perm[i]
                 if self.do_not_look[v1]:
                     continue
-                flg_improve, tmp_tour, diff = self.search_2opt_single_edge(tour, v1)
+                flg_improve, tmp_tour, diff, v4 = self.search_2opt_single_edge(tour, v1)
                 # 改善したら解を更新
                 if flg_improve:
+                    data.plot_tour(tour, placeholder, v1, v4)
                     tour = tmp_tour
-                    # TODO: 効率化
                     self.history.append(self.history[-1] + diff)
                     break
                 else:
@@ -166,10 +200,10 @@ class LocalSearch:
             if not flg_improve:
                 break
 
-            data.plot_tour(tour, placeholder)
+        data.plot_tour(tour, placeholder)
         return tour
 
-    def search_2opt_single_edge(self, tour, v1) -> tuple[bool, list | None, float]:
+    def search_2opt_single_edge(self, tour, v1) -> tuple[bool, list | None, float, int]:
         """一方の交換辺の始点を固定しての2opt近傍の探索
 
         Args:
@@ -179,17 +213,17 @@ class LocalSearch:
         Returns:
             改善解が見つかったかどうかのフラグと、見つかった場合はその改善解と改善量
         """
-        flg_improve, tmp_tour, diff = self.search_2opt_single_end(tour, v1)
+        flg_improve, tmp_tour, diff, v4 = self.search_2opt_single_end(tour, v1)
         # 改善したら解を更新して次の頂点へ
         if flg_improve:
-            return flg_improve, tmp_tour, diff
+            return flg_improve, tmp_tour, diff, v4
         # 逆向きも探索
         tour_reversed = list(reversed(tour))
-        flg_improve, tmp_tour, diff = self.search_2opt_single_end(tour_reversed, v1)
+        flg_improve, tmp_tour, diff, v4 = self.search_2opt_single_end(tour_reversed, v1)
 
-        return flg_improve, (tmp_tour if flg_improve else None), diff
+        return flg_improve, (tmp_tour if flg_improve else None), diff, v4
 
-    def search_2opt_single_end(self, tour, v1) -> tuple[bool, list | None, float]:
+    def search_2opt_single_end(self, tour, v1) -> tuple[bool, list | None, float, int]:
         """一方の交換辺を固定しての2opt近傍の探索
 
         Args:
@@ -197,17 +231,16 @@ class LocalSearch:
             v1: 一方の交換辺の始点
 
         Returns:
-            改善解が見つかったかどうかのフラグと、見つかった場合はその改善解と改善量
+            改善解が見つかったかどうかのフラグと、見つかった場合はその改善解と改善量と交換した辺の情報
         """
         data = self.data
+        dists = self.data.dists
         n = data.n
-        x = data.x
-        y = data.y
         idx_v1 = tour.index(v1)
         idx_v2 = (idx_v1 + 1) % n
         v2 = tour[idx_v2]
-        d12 = dist((x[v1], y[v1]), (x[v2], y[v2]))
-        for d14, v4 in self.dists[v1]:
+        d12 = dists.loc[v1, v2]
+        for v4, d14 in self.dists_list[v1][self.dists_list[v1] < d12].items():
             # d14がd12以上になったら、それ以降は探索しなくていい
             if d14 >= d12:
                 break
@@ -216,17 +249,17 @@ class LocalSearch:
             v3 = tour[idx_v3]
             if len({v1, v2, v3, v4}) != 4:
                 continue
-            d34 = dist((x[v3], y[v3]), (x[v4], y[v4]))
-            d23 = dist((x[v2], y[v2]), (x[v3], y[v3]))
+            d34 = dists.loc[v3, v4]
+            d23 = dists.loc[v2, v3]
 
             if d23 + d14 < d12 + d34:
                 self.do_not_look[v1] = False
                 self.do_not_look[v2] = False
                 self.do_not_look[v3] = False
                 self.do_not_look[v4] = False
-                return True, move_to_2opt_neighbor(tour, idx_v1, idx_v2, idx_v3, idx_v4), d23 + d14 - d12 - d34
+                return True, move_to_2opt_neighbor(tour, idx_v1, idx_v2, idx_v3, idx_v4), d23 + d14 - d12 - d34, v4
 
-        return False, None, 0.0
+        return False, None, 0.0, 0
 
 
 def move_to_2opt_neighbor(tour: list, i1: int, i2: int, i3: int, i4: int) -> list:
